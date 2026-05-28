bd5dc654f300db04db3726aeead5355a46afbfb5
SeinalArifin149 <arifinseinals3@gmail.com>
2026-05-28 20:34:05 +0700

    fix(load_data):change dir

--- app/load_data.py
+++ app/load_data.py
@@ -1,5 +1,12 @@
 import pandas as pd
+from pathlib import Path
+
 def load_data():
-    path = "Top_Pariwisata-Bangkalan.csv"
-    df = pd. read_csv(path)
+
+    BASE_DIR = Path(__file__).resolve().parent
+
+    path = BASE_DIR / "Top_Pariwisata-Bangkalan.csv"
+
+    df = pd.read_csv(path)
+
     return df
