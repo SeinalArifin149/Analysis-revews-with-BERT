1576cd905d0ec263610d176637f5d49168bbe5bb
SeinalArifin149 <arifinseinals3@gmail.com>
2026-05-28 15:50:43 +0700

    fix(add filter sidebar)

--- app/sidebar.py
+++ app/sidebar.py
@@ -1,53 +1,19 @@
 import streamlit as st
-from load_data import load_data
 
-def filter_sidebar():
-
-    # ======================
-    # LOAD DATA
-    # ======================
-    df = load_data()
-
-    # ======================
-    # LABEL TOPIK
-    # ======================
-    topic_labels = {
-        -1: "📌 Umum / Campuran",
-         0: "🏝️ Keindahan Wisata Religi",
-         1: "🚗 Akses dan Tiket Wisata",
-         2: "🕌 Makam Syaikhona Kholil",
-         3: "🤲 Doa dan Religius"
-    }
-
-    # tambah kolom nama topik
-    df["nama_topik"] = df["hasil_topik"].map(topic_labels)
-
-    # ======================
-    # SIDEBAR TITLE
-    # ======================
-    st.sidebar.title("Filter Data")
-
-    # ======================
-    # FILTER SENTIMEN
-    # ======================
+def filter_sidebar(df):
+    # Sentiment
     sentiment_option = st.sidebar.multiselect(
         "Pilih Sentimen",
         options=df["hasil_sentimen"].unique(),
         default=df["hasil_sentimen"].unique()
     )
-
-    # ======================
-    # FILTER TOPIK
-    # ======================
+    # topek
     topik_option = st.sidebar.multiselect(
         "Pilih Topik",
         options=df["nama_topik"].unique(),
         default=df["nama_topik"].unique()
     )
-
-    # ======================
-    # FILTER RATING
-    # ======================
+    # Star
     min_star = int(df["stars"].min())
     max_star = int(df["stars"].max())
 
@@ -58,14 +24,4 @@ def filter_sidebar():
         value=(min_star, max_star)
     )
 
-    # ======================
-    # FILTER DATAFRAME
-    # ======================
-    filtered_df = df[
-        (df["hasil_sentimen"].isin(sentiment_option)) &
-        (df["nama_topik"].isin(topik_option)) &
-        (df["stars"] >= star_range[0]) &
-        (df["stars"] <= star_range[1])
-    ]
-
-    return filtered_df
+    return sentiment_option, topik_option, star_range
