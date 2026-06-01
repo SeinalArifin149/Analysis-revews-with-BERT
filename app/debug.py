e6f9f79c7152d642ce4798e5b05c13adbea411cb
SeinalArifin149 <arifinseinals3@gmail.com>
2026-06-01 23:04:31 +0700

    Feat(debug):debug dimension data

---
+++ app/debug.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("Top_Pariwisata-Bangkalan.csv")
+
+# 1. Cek missing value (nilai kosong) di setiap kolom
+print("--- Jumlah Missing Value per Kolom ---")
+print(df.isna().sum())
+
+print ("---Ini dimensi data")
+print(df.shape)
+
+# 2. Cek jumlah baris data yang duplikat (sama persis)
+# print("\n--- Jumlah Data Duplikat ---")
+# print(df.duplicated().sum())
